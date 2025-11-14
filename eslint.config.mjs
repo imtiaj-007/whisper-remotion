20c526d6397d045a03fffcb955e9362a5206bb3a
SK Imtiaj Uddin <imtiaj.kol@gmail.com>
2025-11-14 11:40:15 +0530

    feat: setup prettier and eslint for enhanced code quality and developer experience

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,22 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+    ...nextVitals,
+    ...nextTs,
+    eslintConfigPrettier,
+    {
+        plugins: {
+            prettier: eslintPluginPrettier,
+        },
+        rules: {
+            'prettier/prettier': 'warn',
+        },
+    },
+    globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+])
 
-export default eslintConfig;
+export default eslintConfig
